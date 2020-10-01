ea217abfcb688bdefc4eb66e1b5886688e545469
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-01 18:56:45 +0530

    snakeladderproblem.java

--- snakeladderproblem.java
+++ snakeladderproblem.java
@@ -1,38 +1,94 @@
 package snakeladderproblem;
 
-public class snakeladderproblem {
+public class uc3snakeladderproblem {
+	public static final int INITIAL_POSITION=0;
+	public static final int NO_OF_PLAYERS=1;
+	public static final int FINAL_POSITION=100;
+	public static final int NO_PLAY=0;
+	public static final int SNAKE=1;
+	public static final int LADDER=2;
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args){
-		int IS_ZERO=0;   int IS_ONE=1;  int IS_TWO=2;  int IS_THREE=3;
-		int IS_FOUR=4;  int IS_FIVE=5;   
-		//Computation
-		double dieresult = Math.floor(Math.random()*10)%6;
-		if (dieresult == IS_ZERO )
-			 System.out.println("Die result is 1");
-		if (dieresult == IS_ONE )
-			 System.out.println("Die result is 2");
-		if (dieresult == IS_TWO )
-			 System.out.println("Die result is 3");
-		if (dieresult == IS_THREE )
-			 System.out.println("Die result is 4");
-		if (dieresult == IS_FOUR )
-			 System.out.println("Die result is 5");
-		if (dieresult == IS_FIVE )
-			 System.out.println("Die result is 6");
+		//Variables
+		int player_one_position =0;
+		int count=0;
 		
-		int IS_NO_PLAY=0;
-		int IS_LADDER=1;
-		int IS_SNAKE=2;
+
 		
-		double dieoption = Math.floor(Math.random()*10)%3;
-		if (dieoption == IS_NO_PLAY )
-			 System.out.println("NO PLAY: DO NOT MOVE");
-		if (dieoption == IS_LADDER )
-			 System.out.println("LADDER: MOVE AHEAD BY RESULT OF DIE");
-		if (dieoption == IS_TWO )
-			 System.out.println("SNAKE: MOVE BEHIND BY RESULT OF DIE");
+
+			
+		while (player_one_position<100) {
+			count++;
+			System.out.println("your count is = "+ count);
+			
+
+			//Computation
+			int dicenumber  =(int) Math.floor((((Math.random()*100)%6)+1));
+			System.out.println("Your dice number is = "+dicenumber);
+
+		
+		int option=(int) Math.floor((Math.random()*100)%3);
+		switch (option) {
+		case  NO_PLAY:
+			 player_one_position= player_one_position;
+			 System.out.println("You got no play you will not move at "+ dicenumber);
 			 
+			 break;
+		case  SNAKE:
+			 player_one_position= player_one_position - dicenumber;
+			 System.out.println("You got snake you will move backward by "+ dicenumber );
+			 
+			
+			 break;
+		case LADDER:
+			player_one_position=player_one_position + dicenumber;
+			System.out.println("You got ladder you will move forward by "+dicenumber );
+			
+			
+			
+			break;
 		}
-}
+		
+		
+		
+		if (player_one_position< 0 ) {
+			player_one_position= 0;
+			System.out.println("Since player position is below zero we start from zero");}
+		
+		if (player_one_position>100 ) {
+			player_one_position-=dicenumber;
+			System.out.println("Since player position is above 100 we stay at same position");}
+			
+		
+		
+
+			
+			
+		
+		 
+	
+		 
+		 System.out.println("the position of the player is = "+ player_one_position);}}
+		
+
+	}
 
 
 
+
+
+	
+		
+		
+		
+		
+			
+		
+		
+		
+		
