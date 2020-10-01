a55a81a8bf5361f65376e9b7b3a82a05df242a31
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-01 18:51:13 +0530

    snakeladderproblem.java

---
+++ snakeladderproblem.java
@@ -0,0 +1,10 @@
+package snakeladderproblem;
+
+public class snakeladderproblem {
+     public static void main(String[] args){
+	      System.out.println("Welcome to Snake Ladder,Start position is 0");
+	
+	}
+}
+
+
